0709d460ef37bb8c6d103da841f659b29d08402b
MadhushaPrasad <madushaprasad21@gmail.com>
2024-06-05 10:38:34 +0530

    calculator divide test case

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,12 +8,17 @@ public class CalculatorTest {
     Calculator calculator;
 
     @BeforeEach
-    void setUp() {
+    public void setUp() {
         calculator = new Calculator();
     }
 
     @Test
     public void testMultiply() {
         assertEquals(20, calculator.multiply(5 , 4));
+    }
+
+    @Test
+    public void testDivide(){
+        assertEquals(5,calculator.divide(25,5));
     }
 }
