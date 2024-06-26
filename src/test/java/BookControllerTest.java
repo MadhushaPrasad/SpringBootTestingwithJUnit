bb3df58533543be6b69246b1f6a8a54c739ea6e0
MadhushaPrasad <madushaprasad21@gmail.com>
2024-06-26 10:16:34 +0530

    setup mockmvc

--- src/test/java/BookControllerTest.java
+++ src/test/java/BookControllerTest.java
@@ -1,14 +1,37 @@
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import com.rest.application.Book;
+import com.rest.application.BookController;
+import com.rest.application.BookRepository;
+import org.junit.Before;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+
 @RunWith(MockitoJUnitRunner.class)
 public class BookControllerTest {
-    private Mockito mockito;
 
     ObjectMapper objectMapper = new ObjectMapper();
     ObjectWriter objectWriter = objectMapper.writer();
+    Book recordOne = new Book(1L, "Harry Potter", "Test Summery", 6);
+    Book recordTwo = new Book(2L, "Harry Potter", "Test Summery", 6);
+    private Mockito mockito;
+    @Mock
+    private BookRepository bookRepository;
+    @InjectMocks
+    private BookController bookController;
 
+    private MockMvc mockMvc;
 
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup().build();
+    }
 }
